a9a66e6892f8692116dae0d1905d5e552960799f
Shani <74494158+shanimarish@users.noreply.github.com>
2025-02-17 05:34:12 +0800

    Create PasswordChecker

---
+++ PasswordChecker/PasswordChecker/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            Console.Write("Enter password again: ");
+            string samePassword = Console.ReadLine();
+
+            if (!password.Equals(string.Empty))
+            {
+                if (!samePassword.Equals(string.Empty)) {
+                    if (password.Equals(samePassword))
+                    {
+                        Console.WriteLine("Passwords match.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Passwords do not match.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a password confirmation.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a password.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
